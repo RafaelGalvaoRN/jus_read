1bef4257c913e530a3d01fd4a5198b8677e84768
RafaelGalvaoRN <rafael.galvao@mprn.mp.br>
2023-10-27 16:10:27 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from utilidades_tesseract import *
+
+st.title("LEITOR DE PDF")
+
+st.markdown("""<br><br> """, unsafe_allow_html=True)
+
+uploaded_file = st.file_uploader("Escolha seu pdf para eu ler", type=["pdf"])
+
+
+
+if uploaded_file is not None:
+    # Lê o conteúdo do arquivo PDF
+    pdf_content = uploaded_file.read()
+
+    # Chama a função para extrair texto do PDF
+    text = extrai_texto(pdf_content)
+
+    # Exibe o texto extraído
+    st.write(text)
+
---
+++ utilidades_tesseract.py
@@ -0,0 +1,39 @@
+import pytesseract
+import fitz
+import time
+from PIL import Image
+pytesseract.pytesseract.tesseract_cmd = r'F:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+
+
+# Função para extrair texto de uma imagem
+def extrai_texto_imagem(imagem):
+    text = pytesseract.image_to_string(imagem, lang='por')
+    return text
+
+# Função para extrair texto de um arquivo PDF
+def extrai_texto(pdf_content):
+    start = time.time()
+    extract = []
+
+    # Use o PyMuPDF para abrir o arquivo PDF
+    pdf_document = fitz.open(stream=pdf_content, filetype="pdf")
+
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document[page_num]
+        image_list = page.get_pixmap()
+
+        # Converta a imagem em um formato suportado pelo Tesseract OCR
+        image_pil = Image.frombytes("RGB", [image_list.width, image_list.height], image_list.samples)
+
+        # Extrair texto de cada página do PDF
+        text = pytesseract.image_to_string(image_pil, lang='por')
+        extract.append(text)
+
+    text = ' '.join(extract).replace('\n', " ")
+    duration = time.time() - start
+    print(f'Duração do método extrair texto: {duration}')
+    print(text)
+
+    return text
